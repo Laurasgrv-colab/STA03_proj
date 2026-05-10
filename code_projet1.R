2d6f57b6b72eaeeef39e30f5cf404d83311665e2
Laura SÁNCHEZ <laura.sanchez@ensta-paris.fr>
2026-05-10 13:31:53 +0200

    questions 2-5

--- code_projet1.R
+++ code_projet1.R
@@ -6,6 +6,8 @@ library(FactoMineR)
 library(factoextra)
 library(cluster)
 library(MASS)
+library(glmnet)
+
 
 #library(Rmixmod)
 #library(alluvial)
@@ -162,28 +164,9 @@ plt2 = plot(res,axes = c(2,3), choix = "var", cex = 0.3, shadow = TRUE)
 cowplot::plot_grid(plt1, plt2, ncol = 2, nrow = 1)
 # Ces plans ne permettent pas de bien discriminer les observations
 
+
 ################
 ### Q3
-################
-
-res2 = hclust(dist(df),method="ward.D2")
-
-p = ncol(df)
-
-pltw =lapply(5, function(k) fviz_cluster(hcut(df[,-p], k = k, hc_method = "ward.D2"),
-                                           main = paste("Ward.D2 - k =", k)))
-pltw
-# à ré-essayer --------------------------------------------------------------------
-
-# indice de silhouette
-sil = silhouette(cutree(res1,k=4), dist(df[, -p]))
-mean(sil[,3])         # indice moyen 
-fviz_silhouette(sil)  # visualisation
-
-# Comparer avec la classification définie par la variable GENRE dont vous tracerez également la silhouette.
-
-################
-### Q3 (2e essai)
 ################
 
 # Calcul de la matrice de distance (nécessaire pour la silhouette)
@@ -233,7 +216,7 @@ cat("Indice de silhouette moyen (Ward k=5) :", mean_sil_ward, "\n")
 cat("Indice de silhouette moyen (GENRE) :", mean_sil_genre, "\n")
 # Indice de silhouette moyen (GENRE) : -0.04031123  
 
-# Les scoressont très bas: les clusters se chevauchent. 
+# Les scores sont très bas: les clusters se chevauchent. 
 # Ceci peut être expliqué en partie par des morceaux de "Jazz-Rock" ou de "Pop-Rock" à cheval entre deux groupes.
 # De plus, le grand nombre de données (168) fait que tous les points 
 # se retrouvent à des distances similaires les uns des autres.
@@ -395,7 +378,9 @@ round(ModAIC$coef,4)
 ################
 library(ROCR)
 
-#df_train_nouveau$GENRE <- droplevels(df_train_nouveau$GENRE) #chat on a vu ça en cours??? 
+df_train_nouveau$GENRE <- droplevels(df_train_nouveau$GENRE) 
+df_test_nouveau$GENRE <- droplevels(df_test_nouveau$GENRE) 
+# pour garder seulement jazz et classical en mémoire (et non pas 3 genres en plus avec 0 observations)
 
 #Sur l'échantillon d'apprentissage : 
 predprobaT_train=predict(ModT,type="response", data = df_train_nouveau)
@@ -412,5 +397,262 @@ plot(ROCT_test,main="ModT Test")
 #Superposition
 plot(ROCT_train,main="Courbes ROC ModT")
 plot(ROCT_test,col="red",add=TRUE)
+lines(c(0,1),c(0,1),lty=2, col = 'blue')          # règle aléatoire 
+segments(x0=0,y0=1,x1=1,y1=1,lty=2 , col = 'green') # règle parfaite
+segments(x0=0,y0=0,x1=0,y1=1,lty=2 , col = 'green') 
+legend(0.5,0.4,legend=c("ModT : Apprentissage", "ModT : Données" , "Règle aléatoire", "Règle parfaite"),col=c("red","black","blue","green"),lty=c(1,1,2,2))
+
 
 #A FINIR
+
+## Autres modeles sur echantillon test
+#ModT
+perfT = performance(predT_test, "auc") # Calcul de l'AUC (Area Under the Curve) (aire sous la courbe)
+AUCT=round(unlist(perfT@y.values),4) 
+plot(ROCT_test,col="purple",main="Courbes ROC : échantillon Test")
+
+
+#Mod1
+predproba1_test=predict(Mod1,type="response",newdata=df_test_nouveau)
+pred1_test = prediction(predproba1_test,df_test_nouveau$GENRE)
+ROC1_test = performance(pred1_test,"tpr","fpr")
+
+perf1 = performance(pred1_test, "auc")
+AUC1=round(unlist(perf1@y.values),4)
+
+plot(ROC1_test,col="red",add=TRUE)
+
+#Mod2
+predproba2_test=predict(Mod2,type="response",newdata=df_test_nouveau)
+pred2_test = prediction(predproba2_test,df_test_nouveau$GENRE)
+ROC2_test = performance(pred2_test,"tpr","fpr")
+
+perf2 = performance(pred2_test, "auc")
+AUC2=round(unlist(perf2@y.values),4)
+
+plot(ROC2_test,col="green",add=TRUE)
+
+#ModAIC
+predprobaAIC_test=predict(ModAIC,type="response",newdata=df_test_nouveau)
+predAIC_test = prediction(predprobaAIC_test,df_test_nouveau$GENRE)
+ROCAIC_test = performance(predAIC_test,"tpr","fpr")
+
+perfAIC = performance(predAIC_test, "auc")
+AUCAIC=round(unlist(perfAIC@y.values),4)
+
+plot(ROCAIC_test,col="black",add=TRUE)
+
+abline(a=0, b=1, lty=2, col="blue") # Règle aléatoire
+segments(x0=0,y0=1,x1=1,y1=1,lty=2 , col = 'grey') # Règle parfaite
+segments(x0=0,y0=0,x1=0,y1=1,lty=2 , col = 'grey') 
+
+legend(0.45,0.6,
+       legend=c(paste("ModT(AUC =", AUCT, ")"), paste("Mod1(AUC =", AUC1, ")"), 
+                         paste("Mod2(AUC =", AUC2, ")"), paste("ModAIC(AUC =", AUCAIC, ")"), 
+                         "Aléatoire (AUC = 0.5)", "Parfaite (AUC = 1)"),
+       col=c("purple", "red","green","black", "blue"),lty=c(1,1,1,1,1, 2))
+
+# L'aire sous la courbe (AUC) la plus élevée est obtenue pour ModAIC (AUC = 0,9633)
+# Sa courbe (en noir) est la plus proche du coin supérieur gauche, 
+# ce qui signifie qu'il offre le meilleur compromis entre le taux de vrais positifs 
+# et le taux de faux positifs.
+# Même s'il est très proche du ModT (AUC = 0,962), 
+# le ModAIC est préférable car il a été optimisé par une procédure de sélection stepwise, 
+# ce qui réduit le nombre de variables et limite les risques de sur-apprentissage.
+
+# Vérifions son adéquation: 
+# Cf. TP9
+
+error_classif=function(data,modele,seuil=0.5)
+{
+  predproba = predict(modele,type="response",newdata=data)
+  glm.pred = ifelse(predproba>seuil,1,0)
+  return(mean((as.numeric(data$GENRE)-1 - glm.pred)^2))
+}
+
+##ModT
+#Apprentissage
+error_classif(df_train_nouveau,ModT) # 0.06839705
+#Test
+error_classif(df_test_nouveau,ModT) # 0.09447771
+##Mod1
+#Apprentissage
+error_classif(df_train_nouveau,Mod1) # 0.1536303
+#Test
+error_classif(df_test_nouveau,Mod1) # 0.1696607
+##Mod2
+#Apprentissage
+error_classif(df_train_nouveau,Mod2) # 0.1459137
+#Test
+error_classif(df_test_nouveau,Mod2) # 0.1556886
+##ModAIC
+#Apprentissage
+error_classif(df_train_nouveau,ModAIC) # 0.07050158
+#Test
+error_classif(df_test_nouveau,ModAIC) # 0.09314704
+
+
+# Sur l'échantillon d'apprentissage, ModT a une erreur légèrement plus faible que ModAIC (0,068 contre 0,070). 
+# C'est normal : ModT utilise plus de variables, il "colle" donc plus aux données d'entraînement.
+# Bien que proche de celle de ModT, l'erreur de classification de ModAIC 
+# reste la plus faible sur l'échantillon de test. Le modèle retenu sera donc ModAIC.
+
+# Remarque: ModAIC était bien plus long à calculer que ModT !
+
+
+
+################
+### Q3
+################
+
+# Avec 168 variables, un modèle classique risque de capturer le "bruit" des données 
+# plutôt que le signal réel. La pénalité $L_2$ de Ridge "écrase" les coefficients vers zéro 
+# sans les annuler, ce qui réduit la complexité du modèle et améliore sa capacité de généralisation sur de nouvelles données.
+# Ridge permet donc de trouver un compromis optimal : on accepte un léger biais 
+# (en ne laissant pas les coefficients s'ajuster parfaitement aux données d'apprentissage) 
+# pour obtenir une réduction massive de la variance, garantissant ainsi des prédictions plus robustes.
+
+library(glmnet)
+
+grid = 10^seq(10, -2, length = 100) # la grille de lambda
+x = as.matrix(df_train_nouveau[,-p])
+y = df_train_nouveau$GENRE
+
+ridge.fit = glmnet(x,y,alpha=0,lambda=grid, family = "binomial")
+
+dim(coef(ridge.fit)) # [1] 168 100
+
+## Cas lambda = 10^10 : très fortement contraint, régularisé
+coef(ridge.fit)[, 1] # Avec 168 coefficients, le résultat est difficilement exploitable
+# Extraction des coefficients pour le premier lambda (le plus grand)
+# On convertit en vecteur numérique et on enlève l'Intercept (ligne 1)
+coeffs_lambda1 <- as.vector(coef(ridge.fit)[, 1])[-1]
+names(coeffs_lambda1) <- rownames(coef(ridge.fit))[-1]
+# On les trie par valeur absolue (pour voir l'importance réelle, positive ou négative)
+coeffs_tries <- sort(abs(coeffs_lambda1), decreasing = TRUE)
+# Affichons les 10 variables les plus importantes
+head(coeffs_tries, 10) # Valeurs très faibles: entre 10^-6 et 10^-8
+# Le modèle prédit presque toujours la moyenne de $y$. 
+# C'est du sous-apprentissage, le biais est très élevé, mais la variance est nulle.
+
+
+# Cas lambda = 10^-2 : pénalité négligeable, liberté totale
+coef(ridge.fit)[, 100]
+# On fait de même:
+coeffs_lambda2 <- as.vector(coef(ridge.fit)[, 100])[-1]
+names(coeffs_lambda2) <- rownames(coef(ridge.fit))[-1]
+# On les trie par valeur absolue (pour voir l'importance réelle, positive ou négative)
+coeffs_tries <- sort(abs(coeffs_lambda2), decreasing = TRUE)
+# Affichons les 10 variables les plus importantes
+head(coeffs_tries, 10) # Valeurs très faibles: entre 10^5 et 10^3
+# proche d'une régression linéaire classique
+# Le modèle est très complexe. Le risque est le sur-apprentissage: 
+# le modèle apprend le "bruit" des données. La variance est élevée
+
+
+plot(ridge.fit, xvar = "lambda", label = TRUE)
+# Plus lambda diminue plus les coefficientss s'expriment et se rapprochent 
+# de la valeur de la regression linéaire classique
+
+
+################
+### Q4
+################
+library(forecast)    
+
+set.seed(123)
+
+x = as.matrix(df_train_nouveau[, colnames(df_train_nouveau) != "GENRE"])
+
+train = sample(1:nrow(x), 2*nrow(x)/3) # proportion 1/3 -test et 2/3 -apprentissage
+test= -train
+
+cv.out = cv.glmnet(x[train,],as.numeric(y[train]) - 1,alpha=0,nfolds = 10,lambda=grid)
+
+# pdf("Fig-Ridge-cv.pdf")
+par(mfrow=c(1,1))
+plot(cv.out)
+# dev.off()
+
+## Algorithme de cv.glmnet:
+# Il effectue une validation croisée en k segments (nfolds = 10):  
+# Premièrement, il divise l'échantillon train en 10 groupes égaux.
+# Pour chaque valeur de lambda de grid, il entraîne le modèle sur 9 groupes 
+# et calcule l'erreur sur le 10ème (groupe de validation).
+# Il répète l'opération 10 fois (chaque groupe sert de test une fois). 
+# Ensuite, Il calcule l'erreur moyenne et son écart-type (les barres grises sur la figure) 
+# pour chaque lambda.
+# Enfin, il retient le lambda qui offre la meilleure stabilité prédictive.
+
+
+## Interprétation du graphique 
+# Partie gauche du graphique (Grand lambda) : L'erreur est élevée et constante. 
+# La pénalité est trop forte, les coefficients sont écrasés vers 0. 
+# Le modèle est trop simple et ne parvient pas à apprendre : c'est le sous-apprentissage.
+
+# Partie droite du graphique (Petit lambda) : On observe que l'erreur (MSE) chute 
+# rapidement à mesure que la pénalité lambda diminue.
+# Le modèle gagne en liberté et commence à capturer 
+# les caractéristiques discriminantes entre le "Jazz" et le "Classical".
+# Le modèle a besoin d'une régularisation faible pour être performant sur ces données. 
+# Le nombre 167 en haut confirme que Ridge conserve toutes les variables, 
+# stabilisant leurs coefficients sans les annuler.
+
+# Les lignes verticales en pointillés :La première ligne à gauche correspond à lambda.min : 
+# c'est la valeur de lambda qui minimise l'erreur de validation croisée.
+# La seconde ligne (plus à droite) correspond à lambda.1se : 
+# c'est le modèle le plus simple (plus régularisé) dont l'erreur reste à moins d'un écart-type du minimum
+
+
+## lambda minimisant l'erreur de prédiction par VC
+meilleur_lam = cv.out$lambda.min # 0.01
+log(meilleur_lam) # -4.60517
+
+# Estimation de l'erreur sur l'échantillon de test
+#ridge.fit = glmnet(x[train,],as.numeric(y[train]),alpha=0,lambda=meilleur_lam)
+#ridge.pred = predict(ridge.fit,s=meilleur_lam,newx=x[test,])
+
+ridge.prob = predict(cv.out, s = meilleur_lam, newx = x[test,], type = "response")
+ridge.pred = ifelse(ridge.prob > 0.5, 1, 0)
+
+y_test_reel = as.numeric(as.factor(df_train_nouveau$GENRE[test])) - 1
+
+erreur_ridge = mean(ridge.pred != y_test_reel)
+print(erreur_ridge) # 0.1051525 (soit environ 10,5% d'erreur)
+# Notons que ModAIC affichait une erreur de 9,31% et ModT environ 9,44%. 
+# Ainsi, obtenir 10,5% avec la régression Ridge semble cohérent.
+
+# Remarque: ModAIC était bien plus long à calculer que Ridge, et les erreurs sont proches...
+
+
+
+
+################
+### Q5
+################
+
+# Idée:
+# À corriger !!!!!
+
+pred_ridge = prediction(ridge.prob, df_test_nouveau$GENRE)
+ROC_ridge = performance(pred_ridge, "tpr", "fpr")
+AUC_ridge = performance(pred_ridge, "auc")@y.values[[1]]
+
+# On trace d'abord la base (ModT Test ou ModAIC Test)
+plot(ROCT_test, col="blue", main="Superposition des courbes ROC (Test)")
+
+# On ajoute la courbe Ridge en une nouvelle couleur (ex: orange)
+plot(ROC_ridge, col="orange", add=TRUE)
+
+# On ajoute les autres si nécessaire (ModAIC par exemple)
+predprobaAIC_test = predict(ModAIC, newdata = df_test_nouveau, type="response")
+predAIC_test = prediction(predprobaAIC_test, df_test_nouveau$GENRE)
+ROCAIC_test = performance(predAIC_test, "tpr", "fpr")
+plot(ROCAIC_test, col="green", add=TRUE)
+
+# 4. Légende pour comparer
+legend("bottomright", legend=c(
+  paste("ModT (AUC =", round(performance(predT_test,"auc")@y.values[[1]], 4), ")"),
+  paste("ModAIC (AUC =", round(performance(predAIC_test,"auc")@y.values[[1]], 4), ")"),
+  paste("Ridge (AUC =", round(AUC_ridge, 4), ")")
+), col=c("blue", "green", "orange"), lty=1)
